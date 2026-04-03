5a225f7036592c6e2be2e062f18050a8b92fe1d4
clay53 <clayton@claytondoesthings.xyz>
2026-04-03 17:39:29 -0400

    RPI publish itself via Avahi

--- rpi-install/configuration.nix
+++ rpi-install/configuration.nix
@@ -63,5 +63,18 @@
     "flakes"
   ];
 
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    publish = {
+      enable = true;
+      addresses = true;
+      domain = true;
+      hinfo = true;
+      userServices = true;
+      workstation = true;
+    };
+  };
+
   system.stateVersion = "25.11"; 
 }
