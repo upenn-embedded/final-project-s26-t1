37b56c7f8ad34546f711b26139a6098fbe660087
clay53 <clayton@claytondoesthings.xyz>
2026-04-06 19:58:40 -0400

    niri convenience hotkeys

--- packages/niri.nix
+++ packages/niri.nix
@@ -1,13 +1,44 @@
-{...}:
+{pkgs, ...}:
 {
-  settings.binds = {
-    "Mod+Shift+E" = {
-      "quit" = [];
+  settings = {
+    binds = {
+      "Mod+Shift+E" = {
+        "quit" = [];
+      };
+      "Mod+Return" = {
+        "spawn" = [
+          "${pkgs.lib.getExe pkgs.alacritty}"
+        ];
+      };
+      "Mod+Q" = {
+        "close-window" = [];
+      };
+      "Mod+F" = {
+        "maximize-window-to-edges" = [];
+      };
+      "Mod+A" = {
+        "focus-column-left" = [];
+      };
+      "Mod+D" = {
+        "focus-column-right" = [];
+      };
+      "Mod+Shift+A" = {
+        "move-column-left" = [];
+      };
+      "Mod+Shift+D" = {
+        "move-column-right" = [];
+      };
+      "Mod+H" = {
+        "show-hotkey-overlay" = [];
+      };
     };
-    "Mod+Return" = {
-      "spawn" = [
-        "alacritty"
-      ];
+
+    layout = {
+      background-color = "#bfbfc1";
+    };
+
+    gestures = {
+      hot-corners.off = [];
     };
   };
 }
